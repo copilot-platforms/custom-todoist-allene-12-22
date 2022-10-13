af2900a0ced7ee8a9341edad6d0e54d840a7e19b
Allene Norton <norton.allene@gmail.com>
2022-10-13 17:47:31 -0400

    init basic nextapp

---
+++ pages/index.js
@@ -0,0 +1,38 @@
+import Link from 'next/link'
+import Head from 'next/head'
+import Container from '../Components/container'
+
+function HomePage(props) {
+    console.log('hello from web app')
+    return (
+        <>
+            <Container>
+                <Head>
+                    <title>Example App</title>
+                </Head>
+                <div>Welcome to Next.js!</div>
+                <Link href="/spaces/first"><a>First Space</a></Link>
+                <div>Name: {props.json.givenName}</div>
+            </Container>
+        </>
+    )
+}
+
+export async function getServerSideProps(context) {
+    const tempId = 'b7db1342-2158-476f-b967-55b6f5aec60d'
+    const headers = {
+        method: 'GET',
+        headers: {
+            "X-API-KEY": process.env.API_KEY,
+            "Content-Type": "application/json"
+        }
+    }
+    const res = await fetch(`https://api-beta.joinportal.com/v1/client/${tempId}`, headers)
+    const json = await res.json()
+    console.log(json)
+    return {
+       props: { json }
+    }
+ }
+
+export default HomePage
