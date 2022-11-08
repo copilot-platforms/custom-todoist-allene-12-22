197748e4a4e788d438618f8293358e2f0a05ff08
Allene Norton <norton.allene@gmail.com>
2022-11-08 17:27:01 -0500

    student dropdown by school seemingly working

--- pages/index.js
+++ pages/index.js
@@ -88,14 +88,11 @@ function HomePage(props) {
 
     // CONDITIONALLY DISPLAY STUDENT LIST BASED ON LOCATION
     const getStudentsByLocation = () => {
-        let studentsByLocation
-        console.log('intetnal location: '+ location)
-        if (location !== ''){studentsByLocation = props.allStudents.filter(student => student.school === location)}
-        console.log(studentsByLocation)
+        let studentsByLocation = props.allStudents.filter(student => student.school === location)
         return <div className='custom-select'>
             <select className="select-selected" onChange={e => { setSelected(e.target.value) }}>
                 <option value="select student">Select Student</option>
-                {props.allStudents.map((student) =>
+                {studentsByLocation.map((student) =>
                     <option key={student.recordId} value={student.recordId}>{student.name}</option>)}
             </select>
         </div>
